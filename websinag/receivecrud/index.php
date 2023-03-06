dcefc820102cb1cadf597f5138dc7895e21966f9
Yna Maurich Gellaco <115850313+YnaMaurichGellaco@users.noreply.github.com>
2023-03-06 21:39:54 +0800

    Add files via upload

---
+++ websinag/receivecrud/index.php
@@ -0,0 +1,74 @@
+<?php
+/* include the class file (global - within application) */
+include_once 'classes/class.user.php';
+include_once 'classes/class.product.php';
+include_once 'classes/class.receive.php';
+include_once 'classes/class.release.php';
+include_once 'classes/class.inventory.php';
+include 'config/config.php';
+
+$page = (isset($_GET['page']) && $_GET['page'] != '') ? $_GET['page'] : '';
+$subpage = (isset($_GET['subpage']) && $_GET['subpage'] != '') ? $_GET['subpage'] : '';
+$action = (isset($_GET['action']) && $_GET['action'] != '') ? $_GET['action'] : '';
+$id = (isset($_GET['id']) && $_GET['id'] != '') ? $_GET['id'] : '';
+
+$user = new User();
+$product = new Product();
+$receive = new Receive();
+$release = new Release();
+$inventory = new Inventory();
+if(!$user->get_session()){
+	header("location: login.php");
+}
+$user_id = $user->get_user_id($_SESSION['user_email']);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>SINAG</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@200&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="css/custom.css?<?php echo time();?>">
+</head>
+<body>
+<div id="container">
+    <div id="header">
+      <h2>SINAG ADMIN PANEL</h2>
+    </div>
+    <div id="wrapper">
+            <div id="menu">
+            <a href="index.php?page=settings&amp;subpage=users"">Employees</a> 
+                <a href="index.php?page=receive">Payroll</a> 
+                <a href="index.php?page=release">Deduction</a>
+                <a href="index.php?page=inventory">Attendance</a>
+                <a href="logout.php" class="move-right">Log Out</a> 
+                <span class="move-right"><?php //echo $user->get_user_lastname($user_id).', '.$user->get_user_firstname($user_id);?>&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;</span> 
+            </div>
+            <div id="content">
+                <?php
+                switch($page){
+                            case 'settings':
+                                require_once 'settings-module/index.php';
+                            break; 
+                            case 'receive':
+                                require_once 'receive-module/index.php';
+                            break; 
+                            case 'inventory':
+                                require_once 'inventory-module/index.php';
+                            break; 
+                            case 'attendance':
+                                require_once 'attendance-module/index.php';
+                            break; 
+                            default:
+                                require_once 'main.php';
+                            break; 
+                        }
+                ?>
+            </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ websinag/receivecrud/inventory-module/menu.php
@@ -0,0 +1,21 @@
+<style type="text/css">
+	.menu{
+		color: white;
+		background-color: black;
+		padding: 10px;
+		font-size: 1.3em;
+		text-decoration: none;
+	}
+	.menu:hover{
+		color:  rgb(209, 133, 34);
+        background-color: #d8c1ae;
+		padding: 10px;
+		
+	}
+</style>
+<ul style="list-style:none;display:inline-block">
+	<li style="float:left">&nbsp;&nbsp;&nbsp;&nbsp;<a href="index.php" class="menu">Add Member</a>&nbsp;&nbsp;&nbsp;&nbsp;</li>
+    <li style="float:left">&nbsp;&nbsp;&nbsp;&nbsp;<a href="getattendance.php" class="menu">Get Attendance</a>&nbsp;&nbsp;&nbsp;&nbsp;</li>
+    <li style="float:left">&nbsp;&nbsp;&nbsp;&nbsp;<a href="printreport.php" class="menu">Print Report</a>&nbsp;&nbsp;&nbsp;&nbsp;</li>
+    <li style="float:left">&nbsp;&nbsp;&nbsp;&nbsp;<a href="viewmember.php" class="menu">View Member</a>&nbsp;&nbsp;&nbsp;&nbsp;</li>
+</ul>
